3ae880797d7fa2924bbedf604546129c521641b8
Camila Santiago <camila.santiago026@gmail.com>
2025-09-04 16:03:55 -0300

    Oitava questão (Lista 01)

---
+++ lista01/oitava.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int valor;
+
+    // Leitura
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &valor);
+
+    // Exibição
+    printf("O sucessor de %d é %d\n", valor, valor+1);
+    printf("O antecessor de %d é %d\n", valor, valor-1);
+
+    return 0;
+}
