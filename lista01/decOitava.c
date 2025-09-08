269b9dd157e0d267eccd59fd1428815d710721ee
Camila Santiago <camila.santiago026@gmail.com>
2025-09-08 20:51:04 -0300

    Décima oitava questão (lista 01)

---
+++ lista01/decOitava.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Exibição
+    printf("************      ***       *           *\n");
+    printf("*          *   *       *   ***       *    *\n");
+    printf("*          *  *         *   *      *        *\n");
+    printf("*          *  *         *   *    *            *\n");
+    printf("*          *  *         *   *   *              *\n");
+    printf("*          *  *         *   *    *            *\n");
+    printf("*          *  *         *   *      *        *\n");
+    printf("*          *   *       *    *        *    *\n");
+    printf("************      ***       *           *\n");
+
+    return 0;
+}
