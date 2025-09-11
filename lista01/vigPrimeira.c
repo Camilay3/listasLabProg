61b28b679c12e3373317fc7fce380c48994bc5f0
Camila Santiago <camila.santiago026@gmail.com>
2025-09-11 09:12:45 -0300

    Vigésima primeira questão (Lista 01)

---
+++ lista01/vigPrimeira.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    // Exibição
+    printf("Os valores inteiros para os caracteres 'A, B, C, a, b, c, 0, 1, 2, $, *, espaço, + e /' são:\n");
+
+    printf("%d\n", 'A');
+    printf("%d\n", 'B');
+    printf("%d\n", 'C');
+    printf("%d\n", 'a');
+    printf("%d\n", 'b');
+    printf("%d\n", 'c');
+    printf("%d\n", '0');
+    printf("%d\n", '1');
+    printf("%d\n", '2');
+    printf("%d\n", '$');
+    printf("%d\n", '*');
+    printf("%d\n", ' ');
+    printf("%d\n", '+');
+    printf("%d\n", '/');
+
+    return 0;
+}
