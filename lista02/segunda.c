2669fa6b517f95c68e3c8da6f13115c896659914
Camila Santiago <camila.santiago026@gmail.com>
2025-09-27 16:13:13 -0300

    Segunda questão (Lista 02)

---
+++ lista02/segunda.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    char numero;
+
+    do {
+        // Leitura
+        printf("Digite um número entre 1 e 7 (0 para cancelar): ");
+        scanf("%hhd", &numero);
+
+        // Lógica
+        if (numero < 0 || numero > 7) {
+            printf("Número inválido\n");
+        } else {
+            switch (numero) {
+                case 1:
+                    printf("Domingo\n");
+                    break;
+                case 2:
+                    printf("Segunda feira\n");
+                    break;
+                case 3:
+                    printf("Terça feira\n");
+                    break;
+                case 4:
+                    printf("Quarta feira\n");
+                    break;
+                case 5:
+                    printf("Quinta feira\n");
+                    break;
+                case 6:
+                    printf("Sexta feira\n");
+                    break;
+                case 7:
+                    printf("Sábado\n");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    } while (numero);
+
+    printf("Operação cancelada\n");
+
+    return 0;
+}
