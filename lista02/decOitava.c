746f402e4e42b7d4d61b50015c199aaccfb0ccc9
Camila Santiago <camila.santiago026@gmail.com>
2025-11-03 09:16:38 -0300

    Décima oitava questão (Lista 02)

---
+++ lista02/decOitava.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long int soma = 0;
+    unsigned long int aux = 1;
+
+    for (int i = 1; i < 64; i++) {
+        soma += aux;
+        aux *= 2;
+    }
+
+    printf("resultado: %ld\n", soma);
+    return 0;
+}
