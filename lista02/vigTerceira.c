f59853cb40e54053c9269f02d0f7ab27320bb8d9
Camila Santiago <camila.santiago026@gmail.com>
2025-11-03 09:28:13 -0300

    Vigésima terceira questão (Lista 02)

---
+++ lista02/vigTerceira.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int teste = 1, contador = 0;
+    if(teste == ++contador) printf("No pré incremento ele incrementa antes e depois compara.\n");
+    
+    teste = 0;
+    contador = 0;
+
+    if(teste == contador++) printf("No pós incremento ele compara e depois incrementa, retornando falso pro if\n");
+    return 0;
+}
