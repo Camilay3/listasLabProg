92c84402dd434c0d4f6a83d6a232f96e6a92ee58
Camila Santiago <camila.santiago026@gmail.com>
2025-11-03 09:04:11 -0300

    Oitava questão (Lista 02)

---
+++ lista02/oitava.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    float valor, entrada;
+    int parcelas;
+
+    printf("Digite o valor da compra: ");
+    scanf("%f", &valor);
+
+    parcelas = valor / 3;
+    entrada = valor - (2 * parcelas);
+
+    printf("Valor da entrada: %.2f\n", entrada);
+    printf("Valor das parcelas: %d\n", parcelas);
+
+    return 0;
+}
